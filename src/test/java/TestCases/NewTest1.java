f750768a7c38074b14e5d90b30ec3a20f9f7447a
badwenikita <badwe.nikita@gmail.com>
2020-02-27 13:07:39 +0530

    First commit

---
+++ src/test/java/TestCases/NewTest1.java
@@ -0,0 +1,11 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class NewTest1 {
+	@Test
+	  public void method1() 
+	  {
+		  System.out.println("Practice class created on Feb 2020");
+	  }
+}
